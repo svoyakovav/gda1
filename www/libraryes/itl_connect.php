8969c7fe9761799bf8165e12d125810c7d888055
Alexei Svoyakov <a.svoyakov@gmail.com>
2021-03-26 15:14:55 +0400

    Первоначальная версия 26_03_2021

---
+++ www/libraryes/itl_connect.php
@@ -0,0 +1,9 @@
+<?php
+	$sid = "(DESCRIPTION = (ADDRESS = (PROTOCOL = TCP)(HOST = TL-SR-GDD1.kuazot.ru)(PORT = 1521)) (CONNECT_DATA = (SERVER = DEDICATED) (SERVICE_NAME = ITL)))";
+	$itl_logon = OCILogon("xx_user", "xx_user2016", $sid, "UTF8");
+	if (!$itl_logon) {
+		$err = OCIError();
+		echo $err['message'];
+		die();
+	}
+?>
